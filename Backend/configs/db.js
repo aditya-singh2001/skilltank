669daa9167b705a00e9e35ac82685a734cd139e2
Flixion <adityasingh200141@gmail.com>
2023-09-27 19:10:59 +0530

    change in backend

--- Backend/configs/db.js
+++ Backend/configs/db.js
@@ -1,4 +1,15 @@
 const mongoose  = require("mongoose");
 require("dotenv").config()
-const connection=mongoose.connect(process.env.dbUrl);
-module.exports={connection}
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.dbUrl);
+      console.log(`MongoDB Connected `);
+  
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+module.exports = {connectDB};
